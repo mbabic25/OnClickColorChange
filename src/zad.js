3309c22b89bc8394a3defa26315bc636c5a85c89
mbabic25 <marija25121995@gmail.com>
2019-06-17 16:51:36 +0200

    Add input field to change button text

--- src/zad.js
+++ src/zad.js
@@ -2,28 +2,39 @@ import React, { Component } from "react";
 
 class Zadatak extends Component {
   state = {
-    colors: ["#000000"]
+    colors: ["#000000"],
+    value: ""
   };
   getUserAsync = async () => {
     let response = await fetch(`http://www.colr.org/json/color/random`);
     let data = await response.json();
     let newColor = data.new_color;
-    this.setState({ colors: [`#${newColor}`, ...this.state.colors] });
+    if (newColor)
+      this.setState({ colors: [`#${newColor}`, ...this.state.colors] });
     console.log("data", data);
     console.log("new state", this.state.colors);
   };
-
+  handleChange = event => {
+    this.setState({ value: event.target.value });
+  };
   render() {
     const lastColor = this.state.colors[0];
 
     return (
       <div className="App">
+        <input
+          className="input-field"
+          type="text"
+          placeholder="Change text..."
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
         <div
           className="button"
           style={{ color: lastColor }}
           onClick={this.getUserAsync}
         >
-          COMPONENT
+          {this.state.value ? this.state.value : "CLICK ME"}
         </div>
         {this.state.colors.length > 1 ? (
           <div className="previous-text-color-text">PREVIOUS TEXT COLORS:</div>
