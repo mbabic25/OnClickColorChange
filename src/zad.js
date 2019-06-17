83485fc46183670ca02f002cce683e11aaa2f581
mbabic25 <marija25121995@gmail.com>
2019-06-17 16:27:55 +0200

    Add the color history container

--- src/zad.js
+++ src/zad.js
@@ -2,19 +2,19 @@ import React, { Component } from "react";
 
 class Zadatak extends Component {
   state = {
-    colors: ["#000"]
+    colors: ["#000000"]
   };
   getUserAsync = async () => {
     let response = await fetch(`http://www.colr.org/json/color/random`);
     let data = await response.json();
     let newColor = data.new_color;
-    this.setState({ colors: [...this.state.colors, `#${newColor}`] });
+    this.setState({ colors: [`#${newColor}`, ...this.state.colors] });
     console.log("data", data);
     console.log("new state", this.state.colors);
   };
 
   render() {
-    const lastColor = this.state.colors[this.state.colors.length - 1];
+    const lastColor = this.state.colors[0];
 
     return (
       <div className="App">
@@ -25,6 +25,17 @@ class Zadatak extends Component {
         >
           COMPONENT
         </div>
+        {this.state.colors.length > 1 ? (
+          <div className="previous-text-color-text">PREVIOUS TEXT COLORS:</div>
+        ) : null}
+        {this.state.colors.map((color, index) => {
+          if (index !== 0)
+            return (
+              <div className="color-text" style={{ color: color }}>
+                {color}
+              </div>
+            );
+        })}
       </div>
     );
   }
